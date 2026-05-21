8321df3eb579ba2b5dccadfce096520aa0b3216a
provokateurin <kate@provokateurin.de>
2026-05-21 09:22:39 +0200

    perf(MailPlugin): Optimize checking group memberships
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- lib/private/Collaboration/Collaborators/MailPlugin.php
+++ lib/private/Collaboration/Collaborators/MailPlugin.php
@@ -4,6 +4,7 @@
  * SPDX-FileCopyrightText: 2017 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+
 namespace OC\Collaboration\Collaborators;
 
 use OC\KnownUser\KnownUserService;
@@ -16,6 +17,7 @@ use OCP\Federation\ICloudIdManager;
 use OCP\IConfig;
 use OCP\IGroupManager;
 use OCP\IUser;
+use OCP\IUserManager;
 use OCP\IUserSession;
 use OCP\Mail\IEmailValidator;
 use OCP\Share\IShare;
@@ -41,6 +43,7 @@ class MailPlugin implements ISearchPlugin {
 		private KnownUserService $knownUserService,
 		private IUserSession $userSession,
 		private IEmailValidator $emailValidator,
+		private IUserManager $userManager,
 		private mixed $shareWithGroupOnlyExcludeGroupsList,
 		private int $shareType,
 	) {
@@ -71,6 +74,7 @@ class MailPlugin implements ISearchPlugin {
 		}
 
 		$currentUserId = $this->userSession->getUser()->getUID();
+		$userGroups = $this->groupManager->getUserGroupIds($this->userSession->getUser());
 
 		$result = $userResults = ['wide' => [], 'exact' => []];
 		$userType = new SearchResultType('users');
@@ -113,26 +117,16 @@ class MailPlugin implements ISearchPlugin {
 					$exactEmailMatch = strtolower($emailAddress) === $lowerSearch;
 
 					if (isset($contact['isLocalSystemBook'])) {
-						if ($this->shareWithGroupOnly) {
-							/*
-							 * Check if the user may share with the user associated with the e-mail of the just found contact
-							 */
-							$userGroups = $this->groupManager->getUserGroupIds($this->userSession->getUser());
-
-							// ShareWithGroupOnly filtering
-							$userGroups = array_diff($userGroups, $this->shareWithGroupOnlyExcludeGroupsList);
-
-							$found = false;
-							foreach ($userGroups as $userGroup) {
-								if ($this->groupManager->isInGroup($contact['UID'], $userGroup)) {
-									$found = true;
-									break;
-								}
-							}
-							if (!$found) {
-								continue;
-							}
+						$contactUser = $this->userManager->get($contact['UID']);
+						if ($contactUser === null) {
+							continue;
 						}
+						$contactGroups = $this->groupManager->getUserGroupIds($contactUser);
+
+						if ($this->shareWithGroupOnly && array_intersect($contactGroups, array_diff($userGroups, $this->shareWithGroupOnlyExcludeGroupsList)) === []) {
+							continue;
+						}
+
 						if ($exactEmailMatch && $this->shareeEnumerationFullMatch) {
 							try {
 								$cloud = $this->cloudIdManager->resolveCloudId($contact['CLOUD'][0] ?? '');
@@ -174,14 +168,7 @@ class MailPlugin implements ISearchPlugin {
 							}
 
 							if (!$addToWide && $this->shareeEnumerationInGroupOnly) {
-								$addToWide = false;
-								$userGroups = $this->groupManager->getUserGroupIds($this->userSession->getUser());
-								foreach ($userGroups as $userGroup) {
-									if ($this->groupManager->isInGroup($contact['UID'], $userGroup)) {
-										$addToWide = true;
-										break;
-									}
-								}
+								$addToWide = array_intersect($contactGroups, $userGroups) !== [];
 							}
 							if ($addToWide && !$this->isCurrentUser($cloud) && !$searchResult->hasResult($userType, $cloud->getUser())) {
 								if ($this->shareType === IShare::TYPE_USER) {
@@ -247,7 +234,7 @@ class MailPlugin implements ISearchPlugin {
 		}
 
 		if ($this->shareType === IShare::TYPE_EMAIL
-				&& !$searchResult->hasExactIdMatch($emailType) && $this->emailValidator->isValid($search)) {
+			&& !$searchResult->hasExactIdMatch($emailType) && $this->emailValidator->isValid($search)) {
 			$result['exact'][] = [
 				'label' => $search,
 				'uuid' => $search,
